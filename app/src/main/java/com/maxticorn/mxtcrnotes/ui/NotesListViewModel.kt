be9d8049a6f7666ef3b4efc2788e09fe5a4e1c5f
maxticorn <letunovskeymax@gmail.com>
2018-06-25 14:26:34 +0500

    load notes

--- app/src/main/java/com/maxticorn/mxtcrnotes/ui/NotesListViewModel.kt
+++ app/src/main/java/com/maxticorn/mxtcrnotes/ui/NotesListViewModel.kt
@@ -6,6 +6,7 @@ import android.arch.lifecycle.MutableLiveData
 import com.maxticorn.mxtcrnotes.NotesApplication
 import com.maxticorn.mxtcrnotes.domain.LoadNotesUseCase
 import com.maxticorn.mxtcrnotes.domain.Note
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class NotesListViewModel(application: Application) : AndroidViewModel(application) {
@@ -17,5 +18,16 @@ class NotesListViewModel(application: Application) : AndroidViewModel(applicatio
     init {
         (application as NotesApplication).appComponent.inject(this)
         notesLiveData.postValue(null)
+        loadNotes()
+    }
+
+    private fun loadNotes() {
+        notesLiveData.postValue(null)
+        loadNotesUseCase.getAllNotes()
+                .observeOn(Schedulers.computation())
+                .subscribeOn(Schedulers.io())
+                .subscribe {
+                    notesLiveData.postValue(it)
+                }
     }
 }
